231b961485b7d1df6adbaf0ba26cf403a2ff2294
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2024-10-01 13:53:57 +0300

    Added ShopItem model
    Created model with the fields that define the desired return of the api

---
+++ WebScraper/Models/ShopItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopScraper.Models
+{
+    internal class ShopItem
+    {
+        public required List<string> Images { get; set; }
+        public string? BrandName { get; set; }
+        public required decimal Price { get; set; }
+        public decimal? Discount { get; set; }
+        public List<string>? Ratings { get; set; }
+        public required string Description { get; set; }
+        public List<string>? BulletPoints { get; set; }
+        public List<string>? Attributes { get; set; }
+    }
+}
