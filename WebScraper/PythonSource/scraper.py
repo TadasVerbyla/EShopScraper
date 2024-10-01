8193e2d7628306b37f3c79a1efb55ee318a39f0c
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2024-10-01 14:16:39 +0300

    Updated scraper
    Changed scraper to use US localization

--- WebScraper/PythonSource/scraper.py
+++ WebScraper/PythonSource/scraper.py
@@ -4,7 +4,10 @@ import argparse
 def main(args):
 
     URL = args.url
-    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+        'Accept-Language': 'en-US'
+    }
     r = requests.get(url=URL, headers=headers)
     print(r.content.decode('utf-8'))
 
