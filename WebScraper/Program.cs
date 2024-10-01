119542b0b2f976c332f4c443125b2b852716e77f
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2024-10-01 14:24:14 +0300

    Service outline
    Linked service with model, added Newtonsoft to work with jsons

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -10,7 +10,7 @@ class Program
     static async Task Main(string[] args)
     {
         var url = "https://www.macys.com/shop/product/calvin-klein-mens-slim-fit-wool-infinite-stretch-suit-pants?ID=2711143&swatchColor=Navy";
-        var result = await ScrapingService.GetInfo(url);
-        Console.WriteLine(result);
+        var item = await ScrapingService.GetInfo(url);
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(item));
     }
 }
--- WebScraper/Services/ScrapingService.cs
+++ WebScraper/Services/ScrapingService.cs
@@ -6,19 +6,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EShopScraper.Helpers;
+using EShopScraper.Models;
 
 namespace EShopScraper.Services
 {
     internal static class ScrapingService
     {
-        public static async Task<string> GetInfo(string url)
+        public static async Task<ShopItem> GetInfo(string url)
         {
             IDocument document = await ScrapeHtml(url);
             if (document == null)
             {
                 throw new ArgumentNullException("Failed to scrape HTML...");
             }
-            return document.Title;
+
+            ShopItem shopItem = new ShopItem()
+            {
+                Images = FindImages(document),
+                BrandName = FindBrandName(document),
+                Price = FindPrice(document),
+                Discount = FindDiscount(document),
+                Ratings = FindRatings(document),
+                Description = FindDescription(document),
+                BulletPoints = FindBulletPoints(document),
+                Attributes = FindAttributes(document),
+            };
+
+            return shopItem;
+        }
+
+        private static List<string> FindImages(IDocument document)
+        {
+            return [];
+        }
+        private static string FindBrandName(IDocument document)
+        {
+            return "";
+        }
+        private static decimal FindPrice(IDocument document)
+        {
+            return 0;
+        }
+        private static decimal FindDiscount(IDocument document)
+        {
+            return 0;
+        }
+        private static List<string> FindRatings(IDocument document)
+        {
+            return [];
+        }
+        private static string FindDescription(IDocument document)
+        {
+            return "";
+        }
+        private static List<string> FindBulletPoints(IDocument document)
+        {
+            return [];
+        }
+        private static List<string> FindAttributes(IDocument document)
+        {
+            return [];
         }
 
         private static async Task<IDocument> ScrapeHtml(string url)
